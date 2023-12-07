b1debfe0937eb64e7b9464d313c2f13ae1438b8d
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-12-07 15:54:57 +0300

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,67 +1,17 @@
-interface Student {
-  firstName: string,
-  lastName: string,
-  age: number,
-  location: string
+interface MajorCredits {
+  credits: number;
+  brand?: string;
 }
 
-const student1: Student = {
-  firstName: 'Paul',
-  lastName: 'Jerry',
-  age: 27,
-  location: 'Nigeria'
+interface MinorCredits  {
+  credits: number;
+  brand: string;
 }
 
-const student2: Student = {
-  firstName: 'Jerry',
-  lastName: 'Parkerson',
-  age: 28,
-  location: 'Edo state'
+export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): number => {
+  return subject1.credits + subject2.credits;
 }
 
-const studentList: Array<Student> = [student1, student2];
-
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-const table: HTMLTableElement = document.createElement('table');
-const thead: HTMLTableSectionElement = document.createElement('thead');
-const th1: HTMLTableCellElement = document.createElement('th');
-const th2: HTMLTableCellElement = document.createElement('th');
-
-th1.innerText = 'First Name';
-th2.innerText = 'Location';
-th1.style.border = '1px solid gray';
-th2.style.border = '1px solid gray';
-th1.style.padding = '.5rem';
-th2.style.padding = '.5rem';
-table.style.border = '1px solid gray';
-table.style.borderCollapse = 'collapse';
-
-
-
-thead.append(th1);
-thead.append(th2);
-
-table.append(thead);
-
-
-studentList.forEach((student) => {
-  const row: HTMLTableRowElement = document.createElement('tr');
-
-  const column1: HTMLTableCellElement = document.createElement('td');
-  const column2: HTMLTableCellElement = document.createElement('td');
-
-  column1.innerText = student.firstName;
-  column2.innerText = student.lastName;
-
-  column1.style.border = '1px solid gray';
-  column2.style.border = '1px solid gray';
-  column1.style.padding = '.5rem';
-  column2.style.padding = '.5rem';
-
-  row.append(column1);
-  row.append(column2)
-
-  table.append(row);
-});
-
-body.append(table)
+export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): number => {
+  return subject1.credits + subject2.credits;
+}
